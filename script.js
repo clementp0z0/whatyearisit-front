b718b2b573ba4c925961ead8290c00052a8a0391
Clément <pozo.clement@gmail.com>
2023-01-20 16:04:49 +0100

    front first

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://whatyearisit-eight.vercel.app/year')
 .then(response => response.json())
 .then(data=> {
-    document.querySelector('#year').innerHTML+= data.year
+    document.querySelector('#year').textContent = data.year
 })
